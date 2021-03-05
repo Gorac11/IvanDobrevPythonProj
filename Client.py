2c0650f87a5b955b3d9d2caa096d758cd1af73b1
Ivan Dobrev <ivand10@abv.bg>
2021-03-05 05:30:21 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -2,14 +2,11 @@ from System import System
 
 
 class Client:
-   # cost = 0
-    _allClients = []
 
     def __init__(self, name):
         self.name = name
         self.cost = 0.0
         self.carsToBuy = 0
-        #self._allClients.append(self)
 
     def see_cars(self):
         System.print_cars(System)
