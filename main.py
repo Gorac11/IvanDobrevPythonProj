a7a442d3ff65dc13ca8265b65ae02d7648cca652
Ivan Dobrev <ivand10@abv.bg>
2021-03-09 01:10:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ client1.rent_car_day("000002")
 client1.rent_car_day("000002")      #Using the same number. Will report as non existant.
 client1.rent_car_hours("000001", 6)
 client1.rent_car_day("000003")
-client2.rent_car_week("000004")
-client2.rent_car_day("000005")
+client1.rent_car_week("000004")     #4 cars => 30% discount
+client2.rent_car_week("000005")
+client2.rent_car_day("000006")
 
 client1.check_out()
 client2.check_out()
