1cbac7c514d319ebbe2ecb1b782e147f409d8756
Ivan Dobrev <ivand10@abv.bg>
2021-03-08 15:38:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ client2 = Client("Eve")
 client1.see_cars()
 
 client1.rent_car_day("000002")
-client1.rent_car_day("000002")
+client1.rent_car_day("000002")      #Using the same number. Will report as non existant.
 client1.rent_car_hours("000001", 6)
 client1.rent_car_day("000003")
 client2.rent_car_week("000004")
